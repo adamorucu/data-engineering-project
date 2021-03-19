a10aeef99af603578789d915bdfb07df997f8a48
adamorucu <adamdorucu@gmail.com>
2021-03-19 12:03:51 +0000

    working hdfs

--- hdfs/start.sh
+++ hdfs/start.sh
@@ -10,6 +10,17 @@ docker service create \
         newnius/hadoop:2.7.4
 
 docker service create \
+        --name hadoop-node4 \
+        --hostname hadoop-node4 \
+        --constraint node.hostname==team15-node4 \
+        --network sparkdemo_spark-net \
+        --endpoint-mode dnsrr \
+        --mount type=bind,src=/home/ubuntu/volume/config,dst=/config/hadoop \
+        --mount type=bind,src=/home/ubuntu/volume/data/hadoop/hdfs,dst=/tmp/hadoop-root \
+        --mount type=bind,src=/home/ubuntu/volume/data/hadoop/logs,dst=/usr/local/hadoop/logs \
+        newnius/hadoop:2.7.4
+
+docker service create \
         --name hadoop-node3 \
         --hostname hadoop-node3 \
         --constraint node.hostname==team15-node3 \
